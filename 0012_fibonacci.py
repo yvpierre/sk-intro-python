e7dd85f3e225da29d48e79aa5e59dcbb1d677731
Pierre <pierre.yvenou@orange.fr>
2025-10-07 11:28:11 +0200

    style: renaming files

--- 0012_fibonacci.py
+++ 0012_fibonacci.py
@@ -11,8 +11,3 @@ while a <= int(n):
     suite.append(a)
     a, b = b, a + b
 print("La suite de Fibonacci jusqu'à", n, "est :", suite)
-
-git commit -m "exos séance 1"
-git branch -M main
-git remote add origin https://github.com/yvpierre/sk-intro-python.git
-git push -u origin main
