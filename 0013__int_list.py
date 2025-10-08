9ddd8c1b8c1df27636130cbc47dc55d237dea8cf
Pierre <pierre.yvenou@orange.fr>
2025-10-08 13:01:59 +0200

    fix: copy list

--- 0013__int_list.py
+++ 0013__int_list.py
@@ -14,20 +14,23 @@ if(val != 'STOP'):
         if(val != 'STOP' and val != ''):
             liste.append(int(val))
 
+liste = [8,2,1,91,3]
 
+print("Liste de base")
+print(liste)
 # On affiche la liste triée
 print("Liste triée")
-sorted = liste 
+sorted = liste.copy() 
 sorted.sort()
 print(sorted)
 
-# Un entier sur deux 
+# # Un entier sur deux 
 print("On affiche un element sur deux")
 for i in range(len(sorted)):
     if i%2 == 0: print(sorted[i])
 
 # La liste originale inversée
 print("on affiche la liste originale à l'envers")
-reversed = liste
+reversed = liste.copy()
 reversed.reverse()
 print(reversed)
