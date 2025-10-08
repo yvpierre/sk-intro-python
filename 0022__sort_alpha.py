acedd7bf9bffb3d0e12deda841174478103888ac
Pierre <pierre.yvenou@orange.fr>
2025-10-08 12:06:13 +0200

    feat: prog 22

---
+++ 0022__sort_alpha.py
@@ -0,0 +1,11 @@
+# Pierre YVENOU
+# 08/10/2025
+# SKEMA BS
+
+## PROGRAMME 22
+def sort_alpha(list):
+    res = list
+    res.sort()
+    return res;
+
+print(sort_alpha(["Alain",'Bernard','Aline', "Xavier","Michel"]))
